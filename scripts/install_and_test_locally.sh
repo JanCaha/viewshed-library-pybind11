68446517a467f673c3b05fc350d8d2a4ff44ae36
Jan Caha <jan.caha@outlook.com>
2023-08-30 14:01:39 +0200

    install to prefix

--- scripts/install_and_test_locally.sh
+++ scripts/install_and_test_locally.sh
@@ -3,6 +3,6 @@ rm -rf build
 rm -rf dist
 
 python3 setup.py bdist_wheel
-pip install dist/viewshed-*.whl # --force-reinstall
+pip install dist/viewshed-*.whl # --force-reinstall --prefix a
 
 pytest -vv -s
