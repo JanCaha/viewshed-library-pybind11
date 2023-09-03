cee156f17debc69097e4edc2410ef373836b7f7b
Jan Caha <jan.caha@outlook.com>
2023-09-04 00:02:34 +0200

    add mask

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,18 @@ def dem(dem_path: Path) -> viewshed.ProjectedSquareCellRaster:
 
 
 @pytest.fixture
+def mask_path() -> Path:
+    return Path(__file__).parent / "data" / "visiblity_mask.tif"
+
+
+@pytest.fixture
+def mask(mask_path: Path) -> viewshed.ProjectedSquareCellRaster:
+    mask = viewshed.ProjectedSquareCellRaster(mask_path.as_posix())
+    assert isinstance(mask, viewshed.ProjectedSquareCellRaster)
+    return mask
+
+
+@pytest.fixture
 def viewpoint(dem: viewshed.ProjectedSquareCellRaster) -> viewshed.Point:
     vp = viewshed.Point(-336428.767, -1189102.785, dem, 1.6)
     assert vp.isValid()
