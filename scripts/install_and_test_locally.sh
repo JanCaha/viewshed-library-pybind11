c86e204b805cb1e1982bb6bd44913f3507cf9968
Jan Caha <jan.caha@outlook.com>
2023-10-25 17:40:17 +0200

    update install process and use venv

--- scripts/install_and_test_locally.sh
+++ scripts/install_and_test_locally.sh
@@ -1,8 +1,37 @@
-pip uninstall viewshed
+#!/bin/bash
+VENV_FOLDER=.venv
+
+if [ -d "$VENV_FOLDER" ];then
+    echo "VENV $VENV_FOLDER exists."
+else
+    echo "Creating the VENV $VENV_FOLDER."
+
+    python3 -m venv $VENV_FOLDER --symlinks # --system-site-packages 
+    source $VENV_FOLDER/bin/activate
+    pip install pytest wheel
+    deactivate
+fi
+
+install_viewshed_package(){
+    pip uninstall viewshed -y
+    python3 setup.py install
+}
+
+source $VENV_FOLDER/bin/activate
+
 rm -rf build
 rm -rf dist
 
-python3 setup.py bdist_wheel
-pip install dist/viewshed-*.whl # --force-reinstall --prefix a
+# install the package
+install_viewshed_package
 
+# regenerate stub file from installed package
+python3 prepare_stub_template.py
+
+# install again with proper stub package
+install_viewshed_package
+
+python3 setup.py bdist_wheel
+
+# run tests
 pytest -vv -s
