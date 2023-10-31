55faa7344eb6ab943a832b32b16b7932b648ded2
Jan Caha <jan.caha@outlook.com>
2023-10-31 14:37:07 +0100

    update

--- scripts/release_to_ppa.sh
+++ scripts/release_to_ppa.sh
@@ -7,12 +7,14 @@ PWD=`pwd`
 
 FOLDER=pybind_viewshed
 PACKAGE=python3-viewshed
-DEBIAN_VERSION=$(grep "python3-viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+")
+MAIN_VERSION=$(grep "python3-viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+")
+DEBIAN_VERSION=$(grep "python3-viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+)?")
 
 rm -rf build dist .mypy_cache .venv
 cd ..
-tar -acf "$PACKAGE"_"$DEBIAN_VERSION".orig.tar.gz $FOLDER
+tar -acf "$PACKAGE"_"$MAIN_VERSION".orig.tar.gz $FOLDER
 cd $FOLDER
 debuild -S -sa
 cd ..
-dput ppa:jancaha/gis-tools "$PACKAGE"_"$DEBIAN_VERSION"-0ppa0_source.changes
+dput ppa:jancaha/gis-tools "$PACKAGE"_"$DEBIAN_VERSION"ppa0_source.changes
+rm $PACKAGE*
