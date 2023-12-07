3d303390d515978481dfee0fb83b034abd53b7ea
Jan Caha <jan.caha@outlook.com>
2023-12-07 11:23:43 +0100

    update

--- scripts/release_to_ppa.sh
+++ scripts/release_to_ppa.sh
@@ -15,11 +15,17 @@ PACKAGE=python3-viewshed
 MAIN_VERSION=$(grep "python3-viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+")
 DEBIAN_VERSION=$(grep "python3-viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+)?")
 
-rm -rf build dist .mypy_cache .venv .py-build-cmake_cache
+rm -rf build dist .mypy_cache .venv .py-build-cmake_cache .pytest_cache
 cd ..
-# tar --exclude='.git' --exclude='tests' --exclude='scripts' --exclude=".github" --exclude=".vscode" --exclude="docker" -acf "$PACKAGE"_"$MAIN_VERSION".orig.tar.gz $FOLDER
+# tar --exclude='.git' \
+#     --exclude=".github" \
+#     --exclude=".vscode" \
+#     --exclude="docker" \
+#     --exclude='scripts' \
+#     --exclude='tests' \
+#     -acf "$PACKAGE"_"$MAIN_VERSION".orig.tar.gz $FOLDER
 cd $FOLDER
-debuild -S -sa
+yes | debuild -S -sa
 cd ..
 dput ppa:jancaha/gis-tools "$PACKAGE"_"$DEBIAN_VERSION"_source.changes --passive
 # rm $PACKAGE*
