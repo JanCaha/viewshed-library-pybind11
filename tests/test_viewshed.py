6b1d8934a8f9642874d4a63343cbde887c4544d7
Jan Caha <jan.caha@outlook.com>
2024-05-20 14:05:51 +0200

    add tests

--- tests/test_viewshed.py
+++ tests/test_viewshed.py
@@ -62,3 +62,25 @@ def test_viewshed_mask(
 
     assert len(file_messages_percent.readlines()) == 84419
     assert len(file_messages_timing.readlines()) == 3
+
+def test_viewshed_visibility_raster(
+    work_folder: Path,
+    dem: viewshed.ProjectedSquareCellRaster,
+    viewpoint: viewshed.Point,
+    mask: viewshed.ProjectedSquareCellRaster,
+    fn_print_percent_done,
+    fn_print_timing,
+    file_messages_percent,
+    file_messages_timing,
+) -> None:
+    algs = viewshed.VisibilityAlgorithms(False)
+
+    v = viewshed.Viewshed(viewpoint, dem, algs)
+    assert isinstance(v, viewshed.Viewshed)
+
+    visibility_raster = work_folder / "visibility_raster.tif"
+
+    v.calculateVisibilityMask()
+    v.saveVisibilityRaster(visibility_raster)
+
+    assert visibility_raster.exists()
