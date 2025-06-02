84bc29fdc2ff2bf85fa9b49c11937c7c92d09ae6
Jan Caha <jan.caha@outlook.com>
2025-06-02 10:32:20 +0200

    update

--- scripts/precommit/build_stubs.sh
+++ scripts/precommit/build_stubs.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+if ! git diff --cached --name-only | grep -q '^src/'; then
+    echo "⏭️ No files from the src directory are staged for commit. No need to rebuild stubs."
+    exit 0
+fi
+
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DWITH_PY_STUBS=ON
 cmake --build build --config Release
